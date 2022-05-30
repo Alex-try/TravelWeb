77b44ac24dc9d9d0446acef9880890aa4cc14292
Alex-try <1065540573@qq.com>
2022-05-30 20:53:20 +0800

    first submit

---
+++ data/content.js
@@ -0,0 +1,27 @@
+const CONTENT_INFO = {
+    island:{
+        h1:"Never Stop",
+        h2:"Explore the World",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat enim eius libero ea asperiores consequuntur autem ut odit? Numquam impedit optio, doloremque dicta soluta esse accusamus quas dolorum? Quis, temporibus."
+    },
+    spring:{
+        h1:"Spring",
+        h2:"Feel the new vitality",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sed nulla libero officiis ducimus enim, voluptatibus obcaecati, eligendi neque odit facilis illo eaque impedit, ipsum ratione reprehenderit accusamus a laborum."
+    },
+    summer:{
+        h1:"Summer",
+        h2:"Enjoy passionate time",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab similique quibusdam dignissimos? Et aliquid molestias quos, illum in, eaque hic modi, repellendus placeat voluptatibus beatae recusandae veniam quisquam? Quibusdam, deleniti."
+    },
+    autumn:{
+        h1:"Autumn",
+        h2:"Experience without limits",
+        p:"Lorem ipsum dolor, sit amet consectetur adipisicing elit. Architecto, vitae quasi dolorum magni voluptates provident aliquam! Tenetur odit quas maxime nam sint unde rem. Minus dignissimos perspiciatis neque soluta ipsum."
+    },
+    winter:{
+        h1:"Winter",
+        h2:"Enjoy peace and freedom",
+        p:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam itaque dolor, adipisci, molestiae veritatis sed debitis distinctio hic temporibus, animi tempore eum perferendis exercitationem deserunt? Tenetur ab aut maxime labore!"
+    },
+}
---
+++ notes.md
@@ -0,0 +1,44 @@
+## section标签
+    语义标签，独立的一块
+
+## css
+1、box-sizing: border-box;
+    设置的边框和内边距的值是包含在width内的。
+    border-box不包含margin
+2、-webkit-user-drag: none;
+    设置用户无法拖拽页面的东西
+3、CSS中的px、em、rem三种单位的关系
+    px：绝对单位，1个单位的px在不同设备上表示是有区别的；有的设备几个颜色点表示1个像素单位。
+    em：是font-size的倍数；当然font-size的值也有几种情况。
+    rem：是相对于根元素html的font-size来计算的。
+4、:root    
+    伪类匹配文档树的根元素，相当于<html>，但优先级更高；
+    声明全局 CSS 变量时 :root 会很有用：
+    例如    :root {
+                --main-color: hotpink;
+                --pane-padding: 5px 42px;
+            }
+5、user-select: none;
+    设置页面字不可选
+6、display: inline-block;
+    与 display: inline 相比，主要区别在于 display: inline-block 允许在元素上设置宽度和高度。
+7、filter:invert(1);
+    CSS属性 filter 将模糊或颜色偏移等图形效果应用于元素。滤镜通常用于调整图像、背景和边框的渲染。
+    invert() 函数反转输入图像。amount 的值定义转换的比例。值为 100% 则图像完全反转。
+8、transition: 0.5s;
+    过渡效果，一个元素在不同状态之间切换的时候定义不同的过渡效果。
+9、<video src="" autoplay muted loop></video>
+    muted 静音
+    loop 循环播放
+10、clip-path: ellipse(0% 0% at 0% 50%);
+    使用裁剪方式创建元素的可显示区域。区域内的部分显示，区域外的隐藏。
+    ellipse(0% 0% at 0% 50%)
+    定义一个椭圆（使用两个半径和一个圆心位置）。
+11、animation: menu_logo ease-in 1s infinite;
+
+12、overflow: hidden;
+
+
+
+
+
